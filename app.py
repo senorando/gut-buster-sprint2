937d12b5aa7ea22a4edccbd21b17dbacb5e44946
Andy Martinez <andymart1999@yahoo.com>
2020-11-06 16:41:28 +0000

    Initialized Basic Flask App

---
+++ app.py
@@ -0,0 +1,49 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os, flask, flask_sqlalchemy, flask_socketio, datetime, pytz
+from flask import request
+from sqlalchemy import func
+
+import models
+#import calories_count
+
+app = flask.Flask(__name__)
+
+app = flask.Flask(__name__)
+
+socketio = flask_socketio.SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+PORT = 8080
+IP = "0.0.0.0"
+
+DOTENV_PATH = join(dirname(__file__), "sql.env")
+load_dotenv(DOTENV_PATH)
+
+DATABASE_URI = os.environ["DATABASE_URL"]
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.init_app(app)
+db.app = app
+
+db.create_all()
+db.session.commit()
+
+@socketio.on("connect")
+def on_connect():
+    print('\nConnected')
+
+@socketio.on("disconnect")
+def on_disconnect():
+    print('\nDisconnected')
+
+@app.route("/")
+def index():
+    return flask.render_template("home.html")
+
+if __name__ == "__main__":
+    socketio.run(
+        app, host=os.getenv("IP", IP), port=int(os.getenv("PORT", PORT)), debug=True
+    )
