b261ea9bff760037891d60b6008aa1c24bd15f46
Andy Martinez <andymart1999@yahoo.com>
2020-11-06 17:27:27 +0000

    Initialized some scripts

---
+++ scripts/GoogleButton.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+import { Socket } from './Socket';
+
+const clientId = 'TODO';
+
+export function Login() {
+  const onSuccess = (res) => {
+    const user = res.profileObj;
+    console.log('|Login Success| currentUser: ', user);
+    Socket.emit('new google', (user));
+  };
+  const onFailure = (res) => {
+    console.log('|Login Failed| res:', res);
+  };
+  return (
+    <div id="GoogleLogin">
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy="single_host_origin"
+      />
+    </div>
+  );
+}
+export function Logout() {
+  const onSuccess = () => {
+    alert('Successfully Logged Out!');
+  };
+  return (
+    <div id="GoogleLogout">
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      />
+    </div>
+  );
+}
