c29e3a18f735dbbd7aa8e11a5043983c31795681
Jinal Patel <jsp88@njit.edu>
2020-11-08 02:24:57 +0000

    update main jsx to render content

---
+++ scripts/Main.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {homeContent } from './Content';
+
+ReactDOM.render(<homeContent/>, document.getElementById('content'));
