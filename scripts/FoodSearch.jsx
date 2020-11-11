f7a8720eeaff650c5cae3c2db936b19000fcb821
atorisemofe <47372162+atorisemofe@users.noreply.github.com>
2020-11-11 17:23:39 -0500

    Update FoodSearch.jsx
    Add search bar to foodsearch page

--- scripts/FoodSearch.jsx
+++ scripts/FoodSearch.jsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import { Socket } from './Socket';
-
+import SearchField from "react-search-field";
+ 
 export function FoodSearch() {
+    function handleSubmit(response) {
+        console.log(response);
+        let name = response;
+        Socket.emit('new search', {
+          'search': name,
+       });
+       console.log('Sent the food ' + name + ' to server!');
+    }
     
+        
     return (
-         <div>Food Search Page
-         </div>
+         <div>
+            <div>Food Search</div>
+            <SearchField
+                  placeholder="Search..."
+                  classNames="test-class"
+                  onSearchClick={handleSubmit}
+                />
+        </div>
         
         );
 }
