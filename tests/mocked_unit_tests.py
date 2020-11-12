5658b3f6a587dad5cfb0ac83d0f764cd59712b88
Nisarg <np575@njit.edu>
2020-11-12 21:52:40 +0000

    adding py files template for unittests

---
+++ tests/mocked_unit_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import unittest.mock as mock
+from os.path import dirname, join
+import sys
+
+sys.path.insert(1, join(dirname(__file__), "../"))
+import app
+
+#funtions todo
+
+if __name__ == "__main__":
+    unittest.main()
