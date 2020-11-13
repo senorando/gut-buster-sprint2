ea858e9010627903609b1de841f4c1c8b0305371
Nisarg <np575@njit.edu>
2020-11-13 15:51:31 +0000

    adding few tests in unmocked file

--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -7,6 +7,23 @@ sys.path.insert(1, join(dirname(__file__), "../"))
 import app
 
 #funtions todo
+class Test(unittest.TestCase):
+    def test_on_connect(self):
+        result = app.on_connect()
+        if result == 1:
+            self.assertTrue(True, "Socket on Connect")
+    
+    def test_on_disconnet(self):
+        result = app.on_disconnect()
+        if result == 1:
+            self.assertTrue(True, "Socket on Disconnect")
+    
+    def test_on_food_search(self):
+        data={
+            'food_search': 'pasta'
+        }
+        result= app.on_new_food_search(data)
+        print(result)
 
 if __name__ == "__main__":
     unittest.main()
