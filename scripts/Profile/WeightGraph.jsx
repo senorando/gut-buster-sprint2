6a1e64508ee9a2bba0052fa5641cd4973d648ba5
Jinal Patel <jsp88@njit.edu>
2020-11-23 19:39:53 +0000

    update line graph feature for profile

--- scripts/Profile/WeightGraph.jsx
+++ scripts/Profile/WeightGraph.jsx
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from 'react';
-
-import { Tooltip, LineChart, Line, XAxis, YAxis, CartesianGrid, Legend,} from "recharts";
-
+import React from 'react';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,} from 'recharts';
 export function WeightGraph(props) {
     const userWeight = props.userWeight;
     
     const weight_data = [
-        { name: "weight1", Weight: userWeight[ userWeight.length - 7] },
-        { name: "weight2", Weight: userWeight[ userWeight.length - 6] },
-        { name: "weight3", Weight: userWeight[ userWeight.length - 5] },
-        { name: "weight4", Weight: userWeight[ userWeight.length - 4] },
-        { name: "weight5", Weight: userWeight[ userWeight.length - 3] },
-        { name: "weight6", Weight: userWeight[ userWeight.length - 2] },
-        { name: "weight7", Weight: userWeight[ userWeight.length - 1] },
+        { name: "weight1", UserWeight: userWeight[ userWeight.length - 7], GoalWeight: 125 },
+        { name: "weight2", UserWeight: userWeight[ userWeight.length - 6], GoalWeight: 125 },
+        { name: "weight1", UserWeight: userWeight[ userWeight.length - 7], GoalWeight: 125 },
+        { name: "weight3", UserWeight: userWeight[ userWeight.length - 5], GoalWeight: 125 },
+        { name: "weight4", UserWeight: userWeight[ userWeight.length - 4], GoalWeight: 125 },
+        { name: "weight5", UserWeight: userWeight[ userWeight.length - 3], GoalWeight: 125 },
+        { name: "weight6", USerWeight: userWeight[ userWeight.length - 2], GoalWeight: 125 },
+        { name: "weight7", UserWeight: userWeight[ userWeight.length - 1], GoalWeight: 125 },
     ];
     return (
         <div className = 'WeightGraph'>
-            <h3>Last 7 Weight Entries</h3>
-            <LineChart width={500} height={129} data={weight_data}>
-                <Line type="monotone" dataKey="Weight" stroke="#8884d8" strokeWidth={2}  labelLine={true} label/>
-            </LineChart>
+            <LineChart
+            width={500}
+            height={300}
+            data={weight_data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5, }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="UserWeight" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="GoalWeight" stroke="#82ca9d" />
+      </LineChart>
         </div>
     )
 }
