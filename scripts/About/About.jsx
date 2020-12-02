5106f04f76641a769c9a1aa52cb9d7a678af98a3
Oreoluwa Akeredolu <oba5@njit.edu>
2020-12-02 16:11:37 +0000

    create landing page

---
+++ scripts/About/About.jsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+
+export function About() {
+    
+    return (
+        <div>
+            <div className = 'a1'>
+                <p>
+                  We aim to provide healthy food instructions,<br/>
+                  diet plans and deliver a live chat accessible from the browser.<br/>
+                  The  web app will provide a social login to allow users to easily identify themselves,<br/>
+                  and feature a Comment/Discussion attitude, through that people can communicate with each other .<br/>
+                </p>
+            </div>
+            <div className = 'a2'>
+                <h1>Team Members</h1>
+                <div className = 'team-intro'>
+                    <div className = 'team-member'>
+                        <h4>Andy Martinez</h4>
+                        <p>
+                            Hello, I'm Oreoluwa Akeredolu. I'm a current student at NJIT persuing a degree in Computer Science.
+                            I enjoy the process of problem solving, which has lead me down the path to absolutely adoring
+                            all aspects of the software development process.  Working on this project has absolutely
+                            exposed me to learning new technoloiges and how it is to truly work in a team enviroment.<br/>
+                            I'm glad to be working with such an awesome team.
+                        </p>
+                    </div>
+                    <div className = 'team-member'>
+                        <h4>Oreoluwa Akeredolu</h4>
+                        <p>
+                            Hello, I'm Oreoluwa Akeredolu. I'm a current student at NJIT persuing a degree in Computer Science.
+                            I enjoy the process of problem solving, which has lead me down the path to absolutely adoring
+                            all aspects of the software development process.  Working on this project has absolutely
+                            exposed me to learning new technoloiges and how it is to truly work in a team enviroment.<br/>
+                            I'm glad to be working with such an awesome team.
+                        </p>
+                    </div>
+                    <div className = 'team-member'>
+                        <h4>Jinal patel</h4>
+                        <p>
+                            Hello, I'm Oreoluwa Akeredolu. I'm a current student at NJIT persuing a degree in Computer Science.
+                            I enjoy the process of problem solving, which has lead me down the path to absolutely adoring
+                            all aspects of the software development process.  Working on this project has absolutely
+                            exposed me to learning new technoloiges and how it is to truly work in a team enviroment.<br/>
+                            I'm glad to be working with such an awesome team.
+                        </p>
+                    </div>
+                    <div className = 'team-member'>
+                        <h4>Nisrag Patel</h4>
+                        <p>
+                            Hello, I'm Oreoluwa Akeredolu. I'm a current student at NJIT persuing a degree in Computer Science.
+                            I enjoy the process of problem solving, which has lead me down the path to absolutely adoring
+                            all aspects of the software development process.  Working on this project has absolutely
+                            exposed me to learning new technoloiges and how it is to truly work in a team enviroment.<br/>
+                            I'm glad to be working with such an awesome team.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className = "a3">
+                <div classname="technologies">
+                    <h3>Technologies</h3>
+                    <ul className="technologies-list">
+                        <li><a href="https://flask.palletsprojects.com/en/1.1.x/" target="_blank">Python Flask</a></li>
+                        <li><a href="https://reactjs.org/" target="_blank">React.js</a></li>
+                        <li><a href="https://www.postgresql.org/" target="_blank">Postgresql</a></li>
+                        <li><a href="https://www.heroku.com/" target="_blank">Heroku</a></li>
+                        <li><a href="https://circleci.com/" target="_blank">Circle CI</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
