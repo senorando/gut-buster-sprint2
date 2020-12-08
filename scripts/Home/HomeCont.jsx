320c1a11846eed6999e958347248e4048c765a2e
Nisarg <np575@njit.edu>
2020-12-08 10:07:23 +0000

    Added details in Home Content

--- scripts/Home/HomeCont.jsx
+++ scripts/Home/HomeCont.jsx
@@ -20,10 +20,16 @@ export function HomeCont(props) {
           }
           <div>
             <ul>
-              <li>Gut Buster is the world's leading fitness application!</li>
-              <li>Once signed in you can view your profile, see your vitals and view recommendations made by our cutting edge software!</li>
-              <li>You can interact with other active users in our live chat!</li>
-            </ul>
+              <li>Gut Buster is a great fitness tool for all your daily fitness needs.</li><br/>
+              <li>Once signed in you can view your profile, you can interact with other active users in our live chat</li><br/>
+              <li>Once logged in you will be promped to fill out the information such as birthday, height, weight, goal weight, and activity level.</li><br/>
+              <li>Navigate to Profile where our cutting edge software! will process all of the information and provide statistical information such as such as pie-chart for your macros, 
+              line graph for goal weight, and the recommended meal based on your macros </li> <br/>
+              <li>Navigate to Workout Log where you will be promped to fill out couple of information such as difficulty level and select workout splits.</li><br/>
+              <li> Once you fill out the information our tool will provide you with workout plan for the full week.</li><br/>
+              <li>Naviagte to the Food Search Page where you can enter any food and our tool will provide you with randomly suggested meals 
+              with calorie counts, and preperation time, and link to the recepie.</li><br/>
+          </ul>
           </div>
         </div>
       }
